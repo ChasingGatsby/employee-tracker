fb833f0d2ee4793f701a0f23f9221e7741837536
Gatsbeau <jabriel0ne@gmail.com>
2023-11-12 18:52:07 -0500

    Finished schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,26 @@ CREATE DATABASE employee_db;
 USE employee_db
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    deptname VARCHAR(30) NOT NULL
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 )
 
 CREATE TABLE role (
-    id INT NOT NULL
-    title 
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(department_name)
+ON DELETE SET NULL
+)
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 )
