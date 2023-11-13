75564dd424ceb7b14440827b5dd84479e0df5fc9
Gatsbeau <jabriel0ne@gmail.com>
2023-11-12 22:48:47 -0500

    Added seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,7 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    role_id INT NOT NULL,
     manager_id INT REFERENCES employee(id),
     FOREIGN KEY (role_id)
     REFERENCES role(id)
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,7 +3,18 @@ VALUES ("Alpha"), ("Beta"), ("Gamma"), ("Delta");
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Fixer", 80000, 1), 
-    ("Breaker", 90000, 2),
-    ("Watcher", 50000,3),
-    ("Sleeper", 85000, 4);
+    ("Breaker", 90000, 1),
+    ("Watcher", 50000,2),
+    ("Sleeper", 85000, 2),
+    ("Spinner", 66662, 3),
+    ("Habituator", 500, 4),
+    ("Janitor", 5000, 4);
 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+Values ('Aiden', 'DeGalles', 4, 7),
+    ('Jasper', 'Castello', 5, null),
+    ('Noelle', 'Pulcinea', 3,1),
+    ('Luciana', 'Roland,', 2,2),
+    ('Sarah', 'Haystrom', 6, null),
+    ('Karen', 'Kolter', 2, 1),
+    ('Graham', 'Pulcinea', 3, null);
